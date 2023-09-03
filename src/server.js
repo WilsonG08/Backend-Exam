0d59a567c0146fb0b3473acfe784d921e8477b25
Wilson-Guayanay <walex2691@gmail.com>
2023-09-02 21:00:05 -0500

    Documentation Swagger

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 import cors from 'cors';
 import routerVeterinarios from './routers/veterinario_routes.js'
 import routerPacientes from './routers/paciente_routes.js'
+// Swagger
+import { swaggerDocs as V1SwaggerDocs } from './v1/swagger.js' 
 
 
 // Inicializaciones
@@ -13,17 +15,23 @@ dotenv.config()
 app.set('port',process.env.port || 3000)
 app.use(cors())
 
+
 // Middlewares 
 app.use(express.json())
 
-
 // Variables globales
-
 
 // Rutas 
 app.use('/api',routerVeterinarios)
 app.use('/api',routerPacientes)
+
+
+// Swagger 
+V1SwaggerDocs(app, app.get('port'))
+
+
 // Manejo de una ruta que no sea encontrada
 app.use((req,res)=>res.status(404).send("Endpoint no encontrado - 404"))
+
 
 export default  app
