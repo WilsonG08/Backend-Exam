88cf683cd18408413f2d0314934c1a58efb413b3
Wilson-Guayanay <walex2691@gmail.com>
2023-09-03 22:10:11 -0500

    paciente

--- src/controllers/paciente_controller.js
+++ src/controllers/paciente_controller.js
@@ -17,15 +17,10 @@ const detallePaciente = async(req,res)=>{
 
 
 const registrarPaciente = async(req,res)=>{
-    if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
-    const nuevoPaciente = new Paciente(req.body)
-    nuevoPaciente.veterinario=req.body.id
-    await nuevoPaciente.save()
-    res.status(200).json({msg:"Registro exitoso del paciente"})
     try{
         if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
-        const {nombre, propietario, email, celular, convencional, ingreso, sintomas, veterinario} = req.body
-        const nuevoPaciente = new Paciente({nombre, propietario, email, celular, convencional, ingreso, sintomas, veterinario})
+        const {nombre, propietario, email, celular, convencional, ingreso, salida, sintomas, veterinario} = req.body
+        const nuevoPaciente = new Paciente({nombre, propietario, email, celular, convencional, ingreso, salida, sintomas, veterinario})
         nuevoPaciente.veterinario=req.body.id
         await nuevoPaciente.save()
         res.status(200).json({msg:"Registro exitoso del paciente"})
@@ -37,11 +32,6 @@ const registrarPaciente = async(req,res)=>{
 
 
 const actualizarPaciente = async(req,res)=>{
-    const {id} = req.params
-    if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
-    if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe el veterinario ${id}`});
-    await Paciente.findByIdAndUpdate(req.params.id,req.body)
-    res.status(200).json({msg:"Actualización exitosa del paciente"})
     try {
         const {id} = req.params
         if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
@@ -55,14 +45,7 @@ const actualizarPaciente = async(req,res)=>{
 }
 
 
-
 const eliminarPaciente = async (req,res)=>{
-    const {id} = req.params
-    if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
-    if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe el veterinario ${id}`})
-    const {salida} = req.body
-    await Paciente.findByIdAndUpdate(req.params.id,{salida:Date.parse(salida),estado:false})
-    res.status(200).json({msg:"Fecha de salida del paciente registrado exitosamente"})
     try{
         const {id} = req.params
         if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
