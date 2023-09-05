0e796aa44544678c582a64d8d439bba12fc0497e
Wilson08 <wilson.guayanay@epn.edu.ec>
2023-09-05 18:52:12 -0500

    paciente

--- src/controllers/paciente_controller.js
+++ src/controllers/paciente_controller.js
@@ -17,18 +17,11 @@ const detallePaciente = async(req,res)=>{
 
 
 const registrarPaciente = async(req,res)=>{
-    try{
-        if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
-        const {nombre, propietario, email, celular, convencional, ingreso, salida, sintomas, veterinario} = req.body
-        const nuevoPaciente = new Paciente({nombre, propietario, email, celular, convencional, ingreso, salida, sintomas, veterinario})
-        nuevoPaciente.veterinario=req.body.id
-        //console.log(req.body)
-        await nuevoPaciente.save()
-        res.status(200).json({msg:"Registro exitoso del paciente"})
-    }
-    catch(error){
-        console.log(error)
-    }
+    if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
+    const nuevoPaciente = new Paciente(req.body)
+    nuevoPaciente.veterinario=req.body.id
+    await nuevoPaciente.save()
+    res.status(200).json({msg:"Registro exitoso del paciente"})
 }
 
 
