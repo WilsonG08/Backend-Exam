d0f342016da96938bc985388dce852b213902290
Wilson08 <wilson.guayanay@epn.edu.ec>
2023-09-05 19:03:14 -0500

    nose

--- src/controllers/paciente_controller.js
+++ src/controllers/paciente_controller.js
@@ -16,14 +16,46 @@ const detallePaciente = async(req,res)=>{
 }
 
 
-const registrarPaciente = async(req,res)=>{
+/* const registrarPaciente = async(req,res)=>{
     if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
+    // request.body
+    // {
+    //     "nombre":"Nose123",
+    //     "propietario":"Perez",
+    //     "email":"nose@gmai.com",
+    //     "celular":"0697854895",
+    //     "convencional":"050257895",
+    //     "ingreso":"02-05-2023",
+    //     "sintomas":"lele panchita"
+    //   }
+    
+    
+    // Crear un nuevo registro 
     const nuevoPaciente = new Paciente(req.body)
-    nuevoPaciente.veterinario=req.body.id
-    await nuevoPaciente.save()
-    res.status(200).json({msg:"Registro exitoso del paciente"})
-}
+    
+    //{"_id":{"$oid":"64f7c10e6a78d8bdd202a9fb"},"nombre":"Metro","propietario":"qw","email":"dani@gmail.com","celular":"12345","convencional":"123456789012345","ingreso":{"$date":{"$numberLong":"1693785958183"}},"sintomas":"svsd","salida":{"$date":{"$numberLong":"1693787189000"}},"estado":false,"veterinario":{"$oid":"64f51f4b66aba3bdcc23284f"},"createdAt":{"$date":{"$numberLong":"1693786198361"}},"updatedAt":{"$date":{"$numberLong":"1693787191158"}},"__v":{"$numberInt":"0"}}
+   console.log(nuevoPaciente)
+   console.log(req.body.id)
 
+    nuevoPaciente.veterinario=req.body._id
+    //await nuevoPaciente.save()
+    //res.status(200).json({msg:"Registro exitoso del paciente"})
+}
+ */
+const registrarPaciente = async(req,res)=>{
+    try{
+        if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
+        const {nombre, propietario, email, celular, convencional, ingreso, salida, sintomas, veterinario} = req.body
+        const nuevoPaciente = new Paciente({nombre, propietario, email, celular, convencional, ingreso, salida, sintomas, veterinario})
+        nuevoPaciente.veterinario=req.body._id
+        //console.log(req.body)
+        await nuevoPaciente.save()
+        res.status(200).json({msg:"Registro exitoso del paciente"})
+    }
+    catch(error){
+        console.log(error)
+    }
+}
 
 const actualizarPaciente = async(req,res)=>{
     try {
